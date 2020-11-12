7e83fe056f32ccf3bc542dcff71871b70846407c
daominhkhanh20 <khanhc1k36@gmail.com>
2020-11-12 16:12:35 +0700

    add database

---
+++ src/sample/Database.java
@@ -0,0 +1,219 @@
+package sample;
+
+import java.sql.*;
+
+
+public class Database {
+    private String driveName;
+    private String url;
+    private String username;
+    private String password;
+    private Connection connection;
+
+    public Database(){
+        this.driveName="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        this.url="jdbc:sqlserver://localhost:1433;databaseName=QUANLYTODANPHO";
+        this.username="sa";
+        this.password="23571113";
+        this.connection=createConnection();
+    }
+    public Connection createConnection(){
+        Connection connection = null;
+        try{
+            Class.forName(getDriveName());
+            try{
+                connection= DriverManager.getConnection(getUrl(),getUsername(),getPassword());
+            }
+            catch (SQLException x){
+                x.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(connection !=null) {
+            System.out.println("Connection successful");
+        }
+        return connection;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDriveName() {
+        return driveName;
+    }
+
+    public void setDriveName(String driveName) {
+        this.driveName = driveName;
+    }
+
+    public Connection getConnection(){
+        return this.connection;
+    }
+    
+
+    public boolean checkLogin(String username,String password)throws SQLException{
+        var query="SELECT * FROM TOTRUONG WHERE USENAME=? AND PASSWORD=?";
+        PreparedStatement preparedStatement=getConnection().prepareStatement(query);
+        ResultSet result=preparedStatement.executeQuery();
+        if(result.next()){
+            return true;
+        }
+        return false;
+    }
+
+    /*
+    * INSERT DATA INTO DATABASE
+    * */
+
+    public void insertUser(String name, String birthday,String phoneNumber,String accommodation) throws SQLException{
+        var query="INSERT INTO NGUOIPHANANH(TEN,NGAYSINH,DIENTHOAI,NOISONG) VALUES(?,?,?,?)";
+        Connection connection=getConnection();
+        PreparedStatement preparedStatement=connection.prepareStatement(query);
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,birthday);
+        preparedStatement.setString(3,phoneNumber);
+        preparedStatement.setString(4,accommodation);
+        preparedStatement.executeUpdate();
+        connection.close();
+    }
+
+    public void createPetitionInDatabase(int idPerson,String content,String day,int quarterOfYear,String classify ,int state) throws SQLException{
+        var query="INSERT INTO DONPHANANH (ID_PERSON,NOIDUNG,NGAY,QUY,PHANLOAI,TRANGTHAI) VALUES(?,?,?,?,?,?)";
+        Connection connection=getConnection();
+        PreparedStatement preparedStatement=connection.prepareStatement(query);
+        preparedStatement.setInt(1,idPerson);
+        preparedStatement.setString(2,content);
+        preparedStatement.setString(3,day);
+        preparedStatement.setInt(4,quarterOfYear);
+        preparedStatement.setString(5,classify);
+        preparedStatement.setInt(6,state);
+        preparedStatement.executeUpdate();
+        connection.close();
+    }
+
+    public ResultSet executeQueryGetIDUser(String name,String phoneNumber,String birthday,String accommodation) throws SQLException{
+        var query="SELECT ID_PERSON FROM NGUOIPHANANH WHERE TEN=? AND DIENTHAOI=? AND NOISONG=? AND NGAYSINH=?";
+        Connection connection=getConnection();
+        PreparedStatement preparedStatement=connection.prepareStatement(query);
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,phoneNumber);
+        preparedStatement.setString(3,accommodation);
+        preparedStatement.setString(4,birthday);
+        return preparedStatement.executeQuery();
+    }
+
+    public int getIdUser(String name,String phoneNumber,String birthday,String accommodation) throws SQLException {
+        ResultSet result=executeQueryGetIDUser(name,phoneNumber,birthday,accommodation);
+        if(result.next()){// if user exist in database
+            var id=result.getInt("ID_PERSON");
+            return id;
+        }
+
+        //insert user into database
+        insertUser(name,birthday,phoneNumber,accommodation);
+        ResultSet result1=executeQueryGetIDUser(name,phoneNumber,birthday,accommodation);
+        var id=result1.getInt("ID_PERSON");
+        return id;
+    }
+    public void insertPetitionIntoDatabase(String name, String birthday,String phoneNumber,String accommodation,
+                              String content,String day,int quarterOfYear,String classify ,int state) throws SQLException{
+        int id=getIdUser(name,phoneNumber,birthday,accommodation);
+        createPetitionInDatabase(id,content,day,quarterOfYear,classify,state);
+    }
+
+
+
+
+    /*
+    * CHANGE STATE FOR THE PETITION
+    * -1: IS A NEW PETITION
+    * 0: THE PETITION IS UNRESOLVED
+    * 1: THE PETITION IS RESOLVED
+    * */
+
+
+    public void changeStatePetition(String name,String phoneNumber,int state) throws SQLException{
+        var query="UPDATE DONPHANH SET TRANGTHAI=? WHERE ID_PERSON IN(SELECT ID_PERSON FROM NGUOIPHANANH WHERE" +
+                "TEN=? AND DIENTHOAI=?)";
+        PreparedStatement preparedStatement=getConnection().prepareStatement(query);
+        preparedStatement.setInt(1,state);
+        preparedStatement.setString(2,name);
+        preparedStatement.setString(3,phoneNumber);
+        preparedStatement.executeUpdate();
+    }
+
+    /**
+     *SEARCH DATA FROM DATABASE
+     */
+
+    public ResultSet getListPetitionResolved() throws SQLException{
+        var query="SELECT TEN,NOISONG,DIENTHOAI,DAY,PHANLOAI,NOIDUNG FROM DONPHANANH INNER JOIN NGUOIPHANANH" +
+                "ON DONPHANANH.ID_PERSON=NGUOIPHANANH.ID_PERSON WHERE TRANGTHAI=1 ORDER BY NGAY DESC";
+        PreparedStatement pre=getConnection().prepareStatement(query);
+        ResultSet result=pre.executeQuery();
+        return result;
+    }
+
+
+    public ResultSet getListPetitionUnsolved()throws SQLException{
+        var query="SELECT TEN,NOISONG,DIENTHOAI,DAY,PHANLOAI,NOIDUNG FROM DONPHANANH INNER JOIN NGUOIPHANANH" +
+                "ON DONPHANANH.ID_PERSON=NGUOIPHANANH.ID_PERSON WHERE TRANGTHAI=0 ORDER BY NGAY DESC";
+        PreparedStatement pre=getConnection().prepareStatement(query);
+        ResultSet result=pre.executeQuery();
+        return result;
+    }
+
+    public ResultSet getListNewPetition()throws SQLException{
+        var query="SELECT TEN,NOISONG,DIENTHOAI,DAY,PHANLOAI,NOIDUNG FROM DONPHANANH INNER JOIN NGUOIPHANANH" +
+                "ON DONPHANANH.ID_PERSON=NGUOIPHANANH.ID_PERSON WHERE TRANGTHAI=-1 ORDER BY NGAY DESC";
+        PreparedStatement pre=getConnection().prepareStatement(query);
+        ResultSet result=pre.executeQuery();
+        return result;
+    }
+
+    public ResultSet getListPetitionForQuarterOfYear(int quarterOfYear) throws SQLException{
+        var query="SELECT TEN,NOISONG,DIENTHOAI,DAY,PHANLOAI,NOIDUNG FROM DONPHANANH INNER JOIN NGUOIPHANANH" +
+                "ON DONPHANANH.ID_PERSON=NGUOIPHANANH.ID_PERSON WHERE QUY=? ORDER BY NGAY DESC";
+        PreparedStatement pre=getConnection().prepareStatement(query);
+        pre.setInt(1,quarterOfYear);
+        ResultSet result=pre.executeQuery();
+        return result;
+    }
+
+    public ResultSet getListPetitionFromTheCondition(String name,String phoneNumber,String day,String classify,int state) throws SQLException{
+        var query="SELECT TEN,NOISONG,DIENTHOAI,DAY,PHANLOAI,NOIDUNG FROM DONPHANANH INNER JOIN NGUOIPHANANH" +
+                "ON DONPHANANH.ID_PERSON=NGUOIPHANANH.ID_PERSON WHERE " +
+                "TEN=? AND DIENTHOAI=? AND NGAY=? AND PHANLOAI=? AND TRANGTHAI=?" +
+                " ORDER BY NGAY DESC";
+        PreparedStatement pre=getConnection().prepareStatement(query);
+        pre.setString(1,name);
+        pre.setString(2,phoneNumber);
+        pre.setString(2,day);
+        pre.setString(3,classify);
+        pre.setInt(4,state);
+        return pre.executeQuery();
+    }
+
+}
