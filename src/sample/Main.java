ed8eb2b26f50718a29c70b2004294140eb75a0d0
boyonepiece <stah432987@gmail.com>
2020-12-16 21:26:36 +0700

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,21 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Fxml/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Fxml/sample_Login.fxml"));
         primaryStage.setTitle("Quản lý thông tin tổ dân phố");
+        primaryStage.getIcons().add(new Image("photo/application.png"));
         Scene scene = new Scene(root, 600, 400);
         //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-        primaryStage.setX(600);
-        primaryStage.setY(300);
+        primaryStage.setX(450);
+        primaryStage.setY(200);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
